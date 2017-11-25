875876db69ff07002782dc3844695471dcd5369f
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2017-11-25 16:58:49 +0100

    added DAO type class User and hibernate mapping of it, to initialize hibernate implementation

---
+++ src/entities/User.java
@@ -0,0 +1,68 @@
+package entities;
+
+/**
+ * @author Johannes
+ * <p>
+ * Modelclass (DAO type class) for user table from database - don't be an idiot about it.
+ * <p>
+ * If problems arise ask author(s) - hibernate is very strict
+ */
+public class User {
+    private int id;
+    private String picture;
+    private String mail;
+    private String password;
+    private String location;
+
+    @SuppressWarnings("Unused")
+    public User() {
+    }
+
+    public User(int id, String picture, String mail, String password, String location) {
+        this.id = id;
+        this.picture = picture;
+        this.mail = mail;
+        this.password = password;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
