90432b0733e7edaed488d04f1cf181cacd6096fd
Johannes Ernstsen <johannes.ernstsen@post.au.dk>
2017-11-30 16:51:42 +0100

    added annotation for Exceptions for better request handling

--- src/dk/stork/exceptions/EntityNotFoundException.java
+++ src/dk/stork/exceptions/EntityNotFoundException.java
@@ -1,8 +1,12 @@
 package dk.stork.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author Johannes Ernstsen
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntityNotFoundException extends RuntimeException {
 
     public EntityNotFoundException(String message) {
