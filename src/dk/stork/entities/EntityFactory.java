6ff102658bf4f4a183db145cde2f05ab2cfc46dc
Johannes Ernstsen <johannes.ernstsen@post.au.dk>
2017-11-30 16:50:55 +0100

    added helper methods to EntityFactory

--- src/dk/stork/entities/EntityFactory.java
+++ src/dk/stork/entities/EntityFactory.java
@@ -1,6 +1,7 @@
 package dk.stork.entities;
 
 import dk.stork.exceptions.EntityNotFoundException;
+import dk.stork.requestHandling.RegisterUserRequest;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -8,6 +9,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Johannes Ernstsen
@@ -22,25 +24,7 @@ public class EntityFactory {
         session = sessionFactory.openSession();
     }
 
-    /**
-     * retrieve user by the use of their email
-     * <p>
-     * CASE IS IGNORED
-     *
-     * @param email email to search for
-     * @return the user associated with the email
-     */
-    public static User getUserFromEmail(String email) {
-        List<User> users = getModelObjects(User.class);
-        for (User user : users) {
-            if (email.equalsIgnoreCase(user.getMail())) {
-                return user;
-            }
-        }
-        throw new EntityNotFoundException("No user found with email: " + email);
-    }
-
-    public static <T> T getModelObject(Class<T> clazz, int id) {
+    public static <T> T getModelObject(int id, Class<T> clazz) {
         if (session == null || !session.isOpen()) {
             initializeSession();
         }
@@ -63,7 +47,7 @@ public class EntityFactory {
 
     }
 
-    public static void save(Object object) {
+    static void save(Object object) {
         if (session == null || !session.isOpen()) {
             initializeSession();
         }
@@ -80,4 +64,72 @@ public class EntityFactory {
         }
     }
 
+    /**
+     * retrieve user by the use of their email
+     * <p>
+     * CASE IS IGNORED
+     *
+     * @param email email to search for
+     * @return the user associated with the email
+     */
+    public static User getUserFromEmail(String email) {
+        List<User> users = getModelObjects(User.class);
+        for (User user : users) {
+            if (email.equalsIgnoreCase(user.getMail())) {
+                return user;
+            }
+        }
+        throw new EntityNotFoundException("No user found with email: " + email);
+    }
+
+    /**
+     * @param sessionId the sessionId for the user to be found
+     * @return the {@link User} with the given sessionId
+     */
+    public static User getUserFromSessionId(String sessionId) {
+        if (sessionId != null && !sessionId.equals("")) {
+            List<User> users = getModelObjects(User.class);
+            for (User user : users) {
+                if (sessionId.equals(user.getSessionId())) {
+                    return user;
+                }
+            }
+        }
+        throw new EntityNotFoundException("No user found with session: " + sessionId);
+    }
+
+    /**
+     * Creates a user in the database from a {@link RegisterUserRequest}
+     *
+     * @param req request with information for creating the user
+     * @return the user
+     */
+    public static int userFromRegisterRequest(RegisterUserRequest req) {
+        User user = new User();
+        user.setName(req.getName());
+        user.setMail(req.getMail());
+        user.setPassword(req.getPassword());
+        user.save();
+        return user.getId();
+    }
+
+    /**
+     * @param id userId of the user to be logged in
+     * @return the sessionId for the user
+     */
+    public static String login(int id) {
+        User user = getModelObject(id, User.class);
+        return login(user);
+    }
+
+    /**
+     * @param user user to be logged in
+     * @return the sessionId for the user
+     */
+    public static String login(User user) {
+        String sessionId = UUID.randomUUID().toString();
+        user.setSessionId(sessionId);
+        user.save();
+        return sessionId;
+    }
 }
